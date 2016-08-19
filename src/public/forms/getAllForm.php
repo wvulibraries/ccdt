38e91026c37930f30473d0ebf9f96fb33d82dfbf
Ajay Krishna Teja Kavuri <ajaykrishnateja@gmail.com>
2016-08-19 16:57:26 -0400

    Pagination added and tests have been passed

--- src/public/forms/getAllForm.php
+++ src/public/forms/getAllForm.php
@@ -8,7 +8,7 @@
   background-color: rgba(240,240,240,1);
     box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
     transition: 0.1s;
-    width: 80%;
+    width: 90%;
     border-radius: 5px;
     padding: 5px 10px;
     margin: 10px;
@@ -37,6 +37,36 @@
   margin: 0px;
   width: 10%;
 }
+
+ul.pagination {
+    display: inline-block;
+    padding: 0;
+    margin: 0;
+}
+
+ul.pagination li {display: inline;}
+
+ul.pagination li a {
+    color: black;
+    float: left;
+    padding: 8px 16px;
+    text-decoration: none;
+    transition: background-color .3s;
+    border: 1px solid #ddd;
+    font-size: 22px;
+}
+
+ul.pagination li a.active {
+    background-color: #003366;
+    color: white;
+    border: 1px solid #003366;
+}
+
+ul.pagination li a:hover:not(.active) {background-color: #ddd;}
+
+.pgDiv{
+  text-align: center;
+}
 </style>
 
 <?php
@@ -47,7 +77,7 @@ $localvars  = localvars::getInstance();
 $db = db::get($localvars->get('dbConnectionName'));
 
 //Some variables
-$rLimit=20;
+$rLimit=25;
 
 //Pagination stuff
 //Get the total number of records
@@ -56,6 +86,7 @@ $numQResult=$db->query($numOfRecQuery);
 if ($numQResult->error()) {
     throw new Exception("ERROR SQL" . $numQResult->errorMsg());
 }
+// Check for the row count or else just fetch
 if ($numQResult->rowCount() < 1) {
    $output .=  "<p>No Rows</p>";
 }
@@ -63,15 +94,24 @@ else{
   $thisRow = $numQResult->fetch();
   $totRecrds= intval($thisRow['totRecrds']);
 }
-if(isset($_GET['pg'])){
-  $curPage = $_GET['pg']+1;
+if(isset($_REQUEST['pg'])){
+  $curPage = intval($_REQUEST['pg'])+1;
+  $offSt=$rLimit*$curPage;
 }
 else{
   $curPage=0;
+  $offSt=0;
 }
 
+//Simple check for the negetive values
+if($curPage<0){
+  $output .=  "<p>No Rows</p>";
+  return;
+}
+
+
 //Execute the query
-$sql = "SELECT * FROM `correspondence` LIMIT 0,$rLimit";
+$sql = "SELECT * FROM `correspondence` LIMIT $offSt,$rLimit";
 $sqlResult = $db->query($sql);
 
 //Check for any errors
@@ -88,7 +128,7 @@ else{
   $output='<div class="cardCont">';
   while($row = $sqlResult->fetch()){
     $thisRow = sprintf(
-    '<a href="http://www.google.com">
+    '<a href="%s?">
     <div class="card">
       <div class="container">
         <h3 class="recId"><b>%s</b></h3>
@@ -98,6 +138,7 @@ else{
       </div>
     </div>
     </a>',
+    htmlentities($_SERVER['PHP_SELF']),
     htmlSanitize($row['ID']),
     htmlSanitize($row['prefix']),
     htmlSanitize($row['first']),
@@ -106,7 +147,7 @@ else{
     htmlSanitize($row['org']),
     htmlSanitize($row['addr1']),
     htmlSanitize($row['addr2']),
-    htmlSanitize($row['City']),
+    htmlSanitize($row['city']),
     htmlSanitize($row['state']),
     htmlSanitize($row['zip']),
     htmlSanitize($row['country'])
@@ -145,6 +186,53 @@ $output.='</div>';
   */
 }
 
+//Set pagination
+$pagination .=  "<hr/>";
+if($curPage==0){
+  $pagination .= sprintf(
+  '
+  <div class="pgDiv">
+  <ul class="pagination">
+    <li><a class="active">Page %s</a></li>
+    <li><a href="%s?pg=%s">Next »</a></li>
+    <li><a href="%s?pg=%s">Last</a></li>
+  </ul>
+  </div>
+  ',
+  htmlSanitize($curPage+1),
+  htmlSanitize($_PHP_SELF),
+  htmlSanitize($curPage),
+  htmlSanitize($_PHP_SELF),
+  htmlSanitize(($totRecrds/$rLimit)-2)
+  );
+}
+else if($curPage>0){
+  $pagination .= sprintf(
+  '
+  <div class="pgDiv">
+  <ul class="pagination">
+    <li><a href="%s?pg=-1">First</a></li>
+    <li><a href="%s?pg=%s">« Prev</a></li>
+    <li><a class="active">Page %s</a></li>
+    <li><a href="%s?pg=%s">Next »</a></li>
+    <li><a href="%s?pg=%s">Last</a></li>
+  </ul>
+  </div>
+  ',
+  htmlSanitize($_PHP_SELF),
+  htmlSanitize($_PHP_SELF),
+  htmlSanitize($curPage-2),
+  htmlSanitize($curPage+1),
+  htmlSanitize($_PHP_SELF),
+  htmlSanitize($curPage),
+  htmlSanitize($_PHP_SELF),
+  htmlSanitize(($totRecrds/$rLimit)-2)
+
+  );
+}
+$pagination .=  "<hr/>";
+
 //Set the results to output variable
 $localvars->set('getAllForm', $output);
+$localvars->set('pagination', $pagination);
 ?>
--- src/public/forms/searchForm.php
+++ src/public/forms/searchForm.php
@@ -1,5 +1,5 @@
 
-<div id="rCssSearch" style="height:auto; margin:0 auto; ">
+<div id="rCssSearch" style="height:auto; margin:0 auto;">
 <form style="width:100%; margin:0 auto;">
   <div class="sDiv" style="float:left;">
     <input style="padding:15px 15px;background:rgba(240,240,240,1);border: 0px solid #dbdbdb;" type="text" placeholder="Search correspondences.." name="rCssSearchBox" required="true">
