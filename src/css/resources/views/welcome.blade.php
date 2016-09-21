0302c4b2acd2f896ed5e3dd163864ccbb1aeb652
pseudoaj <ajaykrishnateja@gmail.com>
2016-09-21 12:59:57 -0400

    added scripts to setup instead of installing everytime

--- src/css/resources/views/welcome.blade.php
+++ src/css/resources/views/welcome.blade.php
@@ -8,13 +8,18 @@
     </head>
     <body>
       <div class="container">
-        <div class="full-container">
-          <div class="quote-div">
-            <h3>Making Data Researchable</h3>
-          </div>
-          <div class="analytics-div">
-            <p>D3.js</p>
-          </div>
+        <div class="jumbotron">
+          <h1>Jumbotron</h1>
+          <p>This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+          <p><a class="btn btn-primary btn-lg">Learn more</a></p>
+        </div>
+      </div>
+      <div class="full-container">
+        <div class="quote-div">
+          <h3>Making Data Researchable</h3>
+        </div>
+        <div class="analytics-div">
+          <p>D3.js</p>
         </div>
       </div>
     </body>
