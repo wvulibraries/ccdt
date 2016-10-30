949784ac3ffc8535a1d6e0c5d2eaceb7ab81f34e
Ajay Krishna Teja Kavuri <ajaykrishnateja@gmail.com>
2016-10-30 19:51:01 -0400

    New controller for home page added

--- src/css/gulpfile.js
+++ src/css/gulpfile.js
@@ -2,18 +2,14 @@ const elixir = require('laravel-elixir');
 
 require('laravel-elixir-vue-2');
 
-/*
- |--------------------------------------------------------------------------
- | Elixir Asset Management
- |--------------------------------------------------------------------------
- |
- | Elixir provides a clean, fluent API for defining some basic Gulp tasks
- | for your Laravel application. By default, we are compiling the Sass
- | file for our application, as well as publishing vendor resources.
- |
- */
-
+// Link for the javascript
 elixir(mix => {
     mix.sass('app.scss')
        .webpack('app.js');
+});
+
+// Link for the bootstrap scss
+elixir(function(mix) {
+  mix.sass('app.scss')
+    .version('css/app.css');
 });
