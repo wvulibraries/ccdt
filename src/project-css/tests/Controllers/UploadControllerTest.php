6d5c1e904c72c5a15472779746af15ad3e74e02e
Tracy McCormick <tracy.alan.mccormick@gmail.com>
2017-10-05 09:04:54 -0400

    removed uncessary test
    removed test that was not needed everything was being done in a 2nd test using a fake_social.txt.

--- src/project-css/tests/Controllers/UploadControllerTest.php
+++ src/project-css/tests/Controllers/UploadControllerTest.php
@@ -37,62 +37,6 @@
 
     }
 
-    public function testUploadFileToTable(){
-      // find admin user
-      $admin = App\User::where('isAdmin', '=', '1')->first();
-
-      //try to import a table without a collection
-      $this->actingAs($admin)
-           ->visit('table/create')
-           ->see('Please create active collection here first')
-           ->assertResponseStatus(200);
-
-      // Generate Test Collection
-      $collection = factory(App\Collection::class)->create([
-          'clctnName' => 'collection1',
-      ]);
-
-      $tblname = 'importtest' . mt_rand();
-
-      $this->visit('table/create')
-           ->type($tblname,'imprtTblNme')
-           ->type('1','colID')
-           ->attach('./storage/app/files/test/mlb_players.csv','fltFile')
-           ->press('Import')
-           ->assertResponseStatus(200)
-           ->see('Edit Schema')
-           ->submitForm('Submit', ['col-0-data' => 'string', 'col-0-size' => 'default',
-                                   'col-1-data' => 'string', 'col-1-size' => 'default',
-                                   'col-2-data' => 'string', 'col-2-size' => 'default',
-                                   'col-3-data' => 'integer', 'col-3-size' => 'default',
-                                   'col-4-data' => 'integer', 'col-4-size' => 'default',
-                                   'col-5-data' => 'integer', 'col-5-size' => 'default'])
-           ->assertResponseStatus(200)
-           ->see('Load Data')
-           ->press('Load Data')
-           ->see('Table(s)')
-           ->assertResponseStatus(200)
-           ->visit('upload/1')
-           ->assertResponseStatus(200)
-           ->see('Upload files to ' . $tblname . ' Table')
-           ->type('test', 'upFldNme')
-           ->attach(array('./storage/app/files/test/test_upload.txt'),'attachments[]')
-           ->press('Upload')
-           ->assertResponseStatus(200)
-           ->see('Upload files to ' . $tblname . ' Table')
-           ->assertFileExists(storage_path('app/' . $tblname . '/test/test_upload.txt'));
-
-      // cleanup remove mlb_players.csv from upload folder
-      Storage::delete('/flatfiles/mlb_players.csv');
-      Storage::delete('/' . $tblname . '/test/test_upload.txt');
-
-      // cleanup remove directory for the test table
-      Storage::deleteDirectory($tblname);
-
-      // drop table after Testing
-      Schema::drop($tblname);
-    }
-
     public function testUploadFileToDisabledTable(){
       // find admin user
       $admin = App\User::where('isAdmin', '=', '1')->first();
@@ -196,6 +140,8 @@
            ->see('Upload files to ' . $tblname . ' Table')
            ->assertFileExists(storage_path('app/' . $tblname . '/test/fake_socials.txt'));
 
+      // The above upload should return 2 messages one for a successful upload
+      // and a second warning that socials were detected in the upload
       $messages = session()->get('messages');
       $this->assertEquals(count($messages), 2, 'Message Count Should Equal 2');
 
