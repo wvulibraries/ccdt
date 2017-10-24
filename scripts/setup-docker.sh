9b011cadd8ee33b1b7cf8c79a5c32d8468a4a995
Tracy McCormick <tracy.alan.mccormick@gmail.com>
2017-10-24 15:24:37 -0400

    update on docker setup
    had to change nginx config to accept large uploads.

--- scripts/setup-docker.sh
+++ scripts/setup-docker.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Setup the environment variables
+cd ../src/project-css
+cp .env.docker .env
+
 docker exec -t rockefellercss_app_1 composer update
 docker exec -t rockefellercss_app_1 php artisan key:generate
 docker exec -t rockefellercss_app_1 php artisan migrate --seed
