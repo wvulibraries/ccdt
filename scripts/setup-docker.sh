263e2615b48fa7b2ae59b7b2daea3ea6635ed84d
Tracy McCormick <tracy.alan.mccormick@gmail.com>
2017-10-25 08:15:25 -0400

    updated copy .env
    changed the line that copied .env.docker to .env to be only one line.

--- scripts/setup-docker.sh
+++ scripts/setup-docker.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 # Setup the environment variables
-cd ../src/project-css
-cp .env.docker .env
+cp ../src/project-css/.env.docker ../src/project-css/.env
 
 docker exec -t rockefellercss_app_1 composer update
 docker exec -t rockefellercss_app_1 php artisan key:generate
