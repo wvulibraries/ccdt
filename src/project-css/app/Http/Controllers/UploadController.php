df6806679c08d76930d88e2db0d854320f9d0b1a
Tracy McCormick <tracy.alan.mccormick@gmail.com>
2017-11-14 15:52:28 -0500

    scrutinizer style changes

--- src/project-css/app/Http/Controllers/UploadController.php
+++ src/project-css/app/Http/Controllers/UploadController.php
@@ -6,13 +6,13 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 use App\Table;
 
-class UploadController extends Controller{
+class UploadController extends Controller {
     /**
      * Create a new controller instance.
      *
      * @return void
      */
-    public function __construct(){
+    public function __construct() {
         $this->middleware('auth');
     }
 
@@ -23,12 +23,12 @@ class UploadController extends Controller{
         // Get the table entry in meta table "tables"
         $curTable = Table::find($curTable);
         if(!$curTable->hasAccess) {
-        return redirect()->route('home')->withErrors(['Table is disabled']);
+        return redirect()->route('home')->withErrors([ 'Table is disabled' ]);
         }
 
         // return the index page
-        return view('admin/upload')->with('tblNme',$curTable->tblNme)
-                                   ->with('tblId',$curTable);
+        return view('admin/upload')->with('tblNme', $curTable->tblNme)
+                                    ->with('tblId', $curTable);
     }
 
     /**
@@ -36,49 +36,48 @@ class UploadController extends Controller{
      *
      * @return Redirect
      */
-    public function storeFiles(Request $request, $curTable)
-    {
-      // set messages array to empty
-      $messages = [];
+    public function storeFiles(Request $request, $curTable) {
+        // set messages array to empty
+        $messages = [];
 
-      // Get the table entry in meta table "tables"
-      $curTable = Table::find($curTable);
-      if(!$curTable->hasAccess){
-        return redirect()->route('home')->withErrors(['Table is disabled']);
-      }
-
-      // Request the file input named 'attachments'
-      $files = $request->file('attachments');
-      $upFldNme = $request->upFldNme;
-
-      //If the array is not empty
-      if ($files[0] != '') {
-        foreach($files as $file) {
-          // Set the destination path
-          $destinationPath = $curTable->tblNme . '/' . $upFldNme;
-          Storage::makeDirectory($destinationPath);
-          // Get the orginal filname or create the filename of your choice
-          $filename = $file->getClientOriginalName();
-
-          if (Storage::exists($destinationPath . '/' . $filename)) {
-            $message = ['content' =>  $filename . ' Already Exists', 'level' =>  'info'];
-          } else {
-             // Copy the file in our upload folder
-             $file->storeAs($destinationPath,$filename);
-
-             $message = [
-                'content'  =>  $filename . ' Upload successful',
-                'level'    =>  'success',
-             ];
-          }
-          array_push($messages, $message);
+        // Get the table entry in meta table "tables"
+        $curTable = Table::find($curTable);
+        if(!$curTable->hasAccess) {
+          return redirect()->route('home')->withErrors([ 'Table is disabled' ]);
         }
 
-        session()->flash('messages', $messages);
-        return view('admin/upload')->with('tblNme',$curTable->tblNme)
-                                   ->with('tblId',$curTable);
-      }
-      return(false);
+        // Request the file input named 'attachments'
+        $files = $request->file('attachments');
+        $upFldNme = $request->upFldNme;
+
+        //If the array is not empty
+        if ($files[ 0 ] != '') {
+          foreach($files as $file) {
+            // Set the destination path
+            $destinationPath = $curTable->tblNme.'/'.$upFldNme;
+            Storage::makeDirectory($destinationPath);
+            // Get the orginal filname or create the filename of your choice
+            $filename = $file->getClientOriginalName();
+
+            if (Storage::exists($destinationPath.'/'.$filename)) {
+              $message = [ 'content' =>  $filename.' Already Exists', 'level' =>  'info' ];
+            } else {
+               // Copy the file in our upload folder
+               $file->storeAs($destinationPath, $filename);
+
+               $message = [
+                  'content'  =>  $filename.' Upload successful',
+                  'level'    =>  'success',
+               ];
+            }
+            array_push($messages, $message);
+          }
+
+          session()->flash('messages', $messages);
+          return view('admin/upload')->with('tblNme', $curTable->tblNme)
+                                     ->with('tblId', $curTable);
+        }
+        return(false);
     }
 
 }
