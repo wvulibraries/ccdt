2069e3ddc82d82d6e3f6b6724c833b51c51ac49e
Tracy McCormick <tracy.alan.mccormick@gmail.com>
2017-11-15 09:04:35 -0500

    removal of unused item

--- src/project-css/app/Http/Controllers/DataViewController.php
+++ src/project-css/app/Http/Controllers/DataViewController.php
@@ -6,7 +6,6 @@ use Response;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Auth;
 use App\Table;
 use App\Libraries\CustomStringHelper;
 use App\Libraries\TikaConvert;
