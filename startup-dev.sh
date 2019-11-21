ff77cb7a46df57ffa2ed8edafd62ed6477516508
Tracy McCormick <tracy.alan.mccormick@gmail.com>
2019-11-21 14:19:19 -0500

    updated startup scripts

--- startup-dev.sh
+++ startup-dev.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+cp .env.dev .env
 composer install
 php artisan key:generate
 nohup php artisan queue:work --daemon --sleep=3 --tries=3 > /dev/null &
--- startup.sh
+++ startup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+cp .env.prod .env
 composer install
 php artisan key:generate
 nohup php artisan queue:work --daemon --sleep=3 --tries=3 > /dev/null &
